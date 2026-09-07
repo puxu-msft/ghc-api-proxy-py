2c1e9b87f1625488a72fed2a0dfafcc897487cad
Pu Xu <puxu@microsoft.com>
2026-09-07 16:33:24 +0000

    fix: import uuid4 in composition root

--- src/app/server/composition.py
+++ src/app/server/composition.py
@@ -11,6 +11,7 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import cast
 from urllib.request import getproxies
+from uuid import uuid4
 
 import httpcore2
 import httpx2
